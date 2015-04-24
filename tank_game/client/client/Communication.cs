c6f2a9ab9720ea508bc9e3a3e2781bf6d4da365a
hasintha <hasintha.12@gmail.com>
2015-04-24 14:50:09 +0530

    client modified

---
+++ tank_game/client/client/Communication.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace client
+{
+   public class Communication
+    {
+
+       String host = "localhost";
+       Int32 inPort = 7000;
+       Int32 outPort = 6000;
+
+        public void sendMsg(String message) {
+
+            try {
+                
+                TcpClient outMsg = new TcpClient(host, outPort);
+
+                Byte[] sentData = System.Text.Encoding.ASCII.GetBytes(message);
+                NetworkStream streamOut = outMsg.GetStream();
+                streamOut.Write(sentData, 0, sentData.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                streamOut.Close();
+                outMsg.Close();
+
+
+
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+ 
+                
+        }
+
+        public void receiveMsg() {
+
+             TcpListener listner=null;
+
+            try {
+
+               listner=new TcpListener(IPAddress.Parse("127.0.0.1"),inPort);
+
+               listner.Start();
+               Byte[] recData = new Byte[256];
+               String response;
+
+               while (true) {
+                   TcpClient inMsg = listner.AcceptTcpClient();
+                   NetworkStream inStream=inMsg.GetStream();
+                   response=String.Empty;
+
+                   int i=0;
+
+
+                   while ((i = inStream.Read(recData, 0, recData.Length)) != 0) {
+                       response = System.Text.Encoding.ASCII.GetString(recData, 0, i);
+                       Console.WriteLine("Received: {0}", response);
+                   
+                   }
+
+                   inStream.Close();
+                   inMsg.Close();
+
+               }
+               
+               
+               
+               
+               
+
+              
+
+            }
+           
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                listner.Stop();
+            }
+ 
+        
+        
+        }
+
+    }
+}
---
+++ tank_game/client/client/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using client;
+
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Communication com = new Communication();
+            com.sendMsg("JOIN#");
+
+           
+
+                com.receiveMsg();
+               
+          
+        }
+    }
+}
